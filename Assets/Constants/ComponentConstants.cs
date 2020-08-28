7c622c5c713ab566235e9ee1d46114ef010f42ba
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-08-28 15:51:20 +0100

    Improved slope animation script

---
+++ Assets/Constants/ComponentConstants.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// perhaps individual components need 
+// their own files later? 
+
+// take screenshot of the file hier. 
+// before hand-in 
+// since they may not appreciate it...
+public class ComponentConstants : MonoBehaviour
+{
+    // multiples of 4 across the board? 
+    public static float tiltDuration = 90f;
+    public static float tiltIncrement = 4f; 
+}
---
+++ Assets/Scripts/Animators/SlopeAnimator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SlopeAnimator : MonoBehaviour
+{
+    // better name?
+    public GameObject slopeHinge;
+
+    // move this to const class?
+    float zDegrees = 90f; 
+
+    // flag for whether its going 
+    // left or right?
+    // (positive or negative zRot)
+
+
+    void Start()
+    {
+        
+    }
+
+    void TiltSlope()
+    {
+        // flag needs to affect this... (not - sign)...
+        StartCoroutine(GradualTilt(-zDegrees));
+
+        // Return the slope to its original rotation
+        StartCoroutine(GradualTilt(zDegrees)); 
+    }
+
+    // 
+    IEnumerator GradualTilt(float zDegrees)
+    {
+        // is int viable here? 
+        // explain dtype choices in writeup*
+        //float timer = ComponentConstants.tiltDuration;
+        float timer = 9f; 
+
+        // make this a const. 
+        // and dynamicv
+        //float delay = timer / 100f; 
+        float delay = 0.01f; 
+
+        while (timer > 0)
+        {
+            // Rotate by increment defined in CC class 
+            slopeHinge.transform.Rotate(new Vector3(0f, 0f, ComponentConstants.tiltIncrement));
+            yield return new WaitForSeconds(delay);
+        }
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Marble")
+        {
+            TiltSlope(); 
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
