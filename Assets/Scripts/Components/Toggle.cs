0e413beb8530a1c80abc5294ed10052e8797f4ab
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-08-30 17:28:41 +0100

    Toggle component upgraded

---
+++ Assets/Scripts/Components/Toggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour
+{
+	// geometry too simple? 
+	
+	
+	
+    void Start()
+    {
+        
+    }
+	
+	void Toggle() 
+	{
+		
+	}
+	
+	// move to utils class 
+	// then apply to all comps. 
+	//
+	IEnumerator GradualRotation() 
+	{
+		timer = 90f; 
+		while (timer > 0) 
+		{
+			// transform.Rotate
+		}
+
+    void Update()
+    {
+        
+    }
+}
