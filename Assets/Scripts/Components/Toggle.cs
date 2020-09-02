9ed609f22221ea9864436bdcb911bc6b93da1fde
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-09-02 09:54:47 +0100

    Toggle component prefab, location, and animator script improved.

--- Assets/Scripts/Components/Toggle.cs
+++ Assets/Scripts/Components/Toggle.cs
@@ -4,33 +4,70 @@ using UnityEngine;
 
 public class Toggle : MonoBehaviour
 {
+	public GameObject trigger0; // right 
+	public GameObject trigger1; // left 
+
 	// geometry too simple? 
-	
-	
-	
-    void Start()
+
+	// byte is the smallest integral type 
+	byte value = 0;
+	bool state;
+
+	// breakout?
+	float zDelta = 90f;
+	float leeway = 1f;
+
+	//// rename 
+	//GameObject marble;
+	//GameObject trigger0;
+	//GameObject trigger1;
+
+	void Start()
     {
-        
-    }
-	
-	void Toggle() 
-	{
-		
+		//marble = GameObject.Find("Marble"); 
+		//trigger0 = GameObject.Find("Trigger0");
+		//trigger1 = GameObject.Find("Trigger1");
 	}
 	
+	public void ToggleState() 
+	{
+		// rotate/static flip 
+		// flip bit 
+		//
+		state = !state;
+
+		// before or after updating state value?
+		//
+		SuddenRotation(state); 
+
+		
+	}
+
 	// move to utils class 
 	// then apply to all comps. 
 	//
-	IEnumerator GradualRotation() 
-	{
-		timer = 90f; 
-		while (timer > 0) 
-		{
-			// transform.Rotate
-		}
+	//IEnumerator GradualRotation()
+	//{
+	//	float timer = 90f;
+	//	while (timer > 0)
+	//	{
+	//		// transform.Rotate
+	//	}
+	//}
+
+	// justify clarity of movement...
+	void SuddenRotation(bool state)
+    {
+		transform.eulerAngles += state ? new Vector3(0f, 0f, zDelta) : new Vector3(0f, 0f, -zDelta); 
+    }
 
     void Update()
     {
-        
+		// need to constrain to x too 
+		//if (marble.transform.position.y <= trigger0.transform.position.y + leeway ||
+		//	marble.transform.position.y <= trigger1.transform.position.y + leeway)
+  //      {
+		//	ToggleState(); 
+  //      }
     }
 }
---
+++ Assets/Scripts/Components/ToggleTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// amalgamate with parent script? 
+// 
+public class ToggleTrigger : MonoBehaviour
+{
+    Toggle toggle; 
+
+    void Start()
+    {
+        toggle = GetComponentInParent<Toggle>(); 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Marble")
+        {
+            toggle.ToggleState(); 
+        }
+    }
+}
