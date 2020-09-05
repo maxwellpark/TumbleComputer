1c2748d0313d86769ebe0677f4b5fb4a1922015c
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-09-05 11:46:39 +0100

    Installation button logic - keep pressed while user installs components on the grid

---
+++ Assets/Scripts/UI/InstallationButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// could change buttons to toggles, then when one is
+// pressed, toggle off all the others
+// (or at least the currently on toggle)
+// 
+public class InstallationButton : MonoBehaviour
+{
+
+    // The buttons that comprise the installation menu 
+    [SerializeField] private Button[] buttons;
+
+    public void SetAllButtonsInteractable()
+    {
+        foreach (Button button in buttons)
+        {
+            button.interactable = true;
+        }
+    }
+
+    public void OnButtonClicked(Button clickedButton)
+    {
+        int buttonIndex = System.Array.IndexOf(buttons, clickedButton);
+
+        if (buttonIndex == -1)
+            return;
+
+        SetAllButtonsInteractable();
+
+        clickedButton.interactable = false;
+    }
+}
---
+++ Assets/Scripts/UI/InstallationManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstallationManager : MonoBehaviour
+{
+    public HardwareComponent componentBeingInstalled; // priv?
+    
+
+}
